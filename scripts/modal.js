50f9dca1b066fa8653fb91643fab9813d6bb0f5f
epsilon11101 <amirandav1991@gmail.com>
2022-08-14 02:52:36 -0500

    ended modal script and design

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,7 +1,17 @@
+(() => {
+
 let btn_modal = document.querySelector('input[type="button"]');
+let modal = document.getElementsByClassName('modal-container');
+let modal_close_button = document.querySelector('.modal-container .modal-card i');
 
 
 
-btn_modal.addEventListener('click', function(){
-    console.log("click")
-});
+let closeModal = () => modal[0].classList.toggle('modal-hidden');   
+
+btn_modal.addEventListener('click', closeModal);
+modal_close_button.addEventListener('click', closeModal);
+
+
+
+
+})();
